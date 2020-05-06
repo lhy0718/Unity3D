5d07ae1776b48470a49e1dafc5e1e07606c2fdd5
HANYONG LEE <glhy0718@gmail.com>
2020-05-06 22:04:01 +0900

    init

---
+++ GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Util_LHY
+{
+	public static class Util
+	{
+		/// <summary>
+		///   <para>Determine whether two float variables are equal within the range of threshold.</para>
+		/// </summary>
+		/// <param name="value1"></param>
+		/// <param name="value2"></param>
+		/// <param name="threshold"></param>
+		public static bool FloatEquals(float value1, float value2, float threshold = 0.1f) => Mathf.Abs(value1 - value2) < threshold;
+
+		/// <summary>
+		///   <para>Prints multiple parameters.</para>
+		/// </summary>
+		public static void Print(params object[] message)
+		{
+			string s = "";
+			foreach (object m in message)
+			{
+				s += m.ToString() + ", ";
+			}
+			MonoBehaviour.print(s.Remove(s.Length - 2));
+		}
+
+		public static float NormalizeAngle(float angle)
+		{
+			while (angle > 180) angle -= 360;
+			while (angle < -180) angle += 360;
+			return angle;
+		}
+	}
+}
